7456e1a2b91e3f1cc88011ad68e5f3cd3f7499ec
Andrey <219523356+designedbyandrey@users.noreply.github.com>
2026-02-11 21:42:54 +0100

    Finalize the main logic of the project eg. Initial grid load and grid change based on prompt input

--- main.js
+++ main.js
@@ -1,19 +1,73 @@
-document.querySelector(".btn-main")
+/*
+1. Event btn click
+2. Erase old grid
+3. Let user input new value
+3. Add with setting to grid child (grid width/input value)
+4. Changing i value bases on the input value
 
+*/
 
-let grid = document.querySelector(".grid")
-
+let grid = document.querySelector(".grid");
+let mainButton = document.querySelector(".btn-main");
+let gridWidth = 740;
 
 for (let i = 0; i < 256; i++){
-    let gridChild = document.createElement("div")
-    gridChild.classList.add("grid-item")
-    grid.appendChild(gridChild)
-    
-    gridChild.addEventListener("mouseover", () => {
-    gridChild.classList.add("grid-hover")
-    })
+        let gridChild = document.createElement("div")
+        gridChild.classList.add("grid-item")
 
-    gridChild.addEventListener("mouseout", () => {
-    gridChild.classList.remove("grid-hover")
-    })
+        grid.appendChild(gridChild);
+        
+        gridChild.addEventListener("mouseover", () => {
+        gridChild.classList.add("grid-hover")
+        })
+
+        gridChild.addEventListener("mouseout", () => {
+        gridChild.classList.remove("grid-hover")
+        })
+    }
+
+function resetGrid() {
+    
+    deleteGrid()
+
+    let isValid = false;
+    let inputValue;
+
+    while(isValid === false) {
+        inputValue = parseInt(prompt("Squares per side (max 100)"))
+        if (!isNaN(inputValue) && inputValue <= 100 && inputValue > 0) {
+            isValid = true;
+        } else {
+            alert("Please enter a valid number ")
+        }
+    }
+    
+
+
+    for (let i = 0; i < inputValue * inputValue; i++){
+        let gridChild = document.createElement("div")
+        gridChild.classList.add("grid-item")
+        let gridChildWidth = gridWidth/inputValue; 
+
+        gridChild.style.width = `${gridChildWidth}px`
+        gridChild.style.height = `${gridChildWidth}px`
+        grid.appendChild(gridChild);
+        
+        gridChild.addEventListener("mouseover", () => {
+        gridChild.classList.add("grid-hover")
+        })
+
+        gridChild.addEventListener("mouseout", () => {
+        gridChild.classList.remove("grid-hover")
+        })
+    }
+
 }
+
+function deleteGrid() {
+    grid.innerHTML = ''
+}
+
+mainButton.addEventListener ("click", () => {
+    resetGrid()
+})
